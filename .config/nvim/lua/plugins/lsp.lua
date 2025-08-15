de43a1e691b525fc0039a556339cd39cd306b32c
Arijitchkc <chakrabortyarijit1315@gmail.com>
2025-08-15 09:56:19 +0100

    tmux

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -133,6 +133,7 @@ return {
             },
           },
         },
+        pyright = {},
       },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
